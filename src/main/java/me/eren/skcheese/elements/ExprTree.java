b8a13cc2bad03f0d161749587be5415e123bd92c
Eren KARA <karaeren362@gmail.com>
2023-12-28 20:14:57 +0300

    added tree expression

---
+++ src/main/java/me/eren/skcheese/elements/ExprTree.java
@@ -0,0 +1,80 @@
+package me.eren.skcheese.elements;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.*;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+
+import java.util.*;
+
+public class ExprTree extends SimpleExpression<String> {
+
+    static {
+        Skript.registerExpression(ExprTree.class, String.class, ExpressionType.COMBINED, "tree of %~objects%");
+    }
+
+    private Variable<?> variable;
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        if (!(exprs[0] instanceof Variable<?>)) {
+            Skript.error("The tree expression can only be used with variables.");
+            return false;
+        }
+        variable = (Variable<?>) exprs[0];
+        if (!variable.isList()) {
+            Skript.error("You can only get the tree of a list variable.");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected String[] get(Event e) {
+        return getAllKeys((TreeMap<Object, Object>) variable.getRaw(e));
+    }
+
+    @Override
+    public boolean isSingle() {
+        return false;
+    }
+
+    @Override
+    public Class<? extends String> getReturnType() {
+        return String.class;
+    }
+
+    @Override
+    public String toString(Event e, boolean debug) {
+        return "tree of " + variable;
+    }
+
+    @Override
+    public boolean isLoopOf(String s) {
+        return s.equals("branch");
+    }
+
+    public String[] getAllKeys(TreeMap<Object, Object> map) {
+        ArrayList<String> keysList = getAllKeysHelper(map, "");
+        return keysList.toArray(new String[0]);
+    }
+
+    public ArrayList<String> getAllKeysHelper(TreeMap<Object, Object> map, String prefix) {
+        ArrayList<String> keys = new ArrayList<>();
+
+        for (Map.Entry<Object, Object> entry : map.entrySet()) {
+            String currentKey = entry.getKey() == null ? "" : entry.getKey().toString();
+            String keyPath = prefix.isEmpty() ? currentKey : prefix +
+                    (currentKey.isEmpty() ? "" : Variable.SEPARATOR + currentKey);
+
+            if (entry.getValue() instanceof TreeMap) {
+                keys.addAll(getAllKeysHelper((TreeMap<Object, Object>) entry.getValue(), keyPath));
+            } else if (!keyPath.isEmpty()) {
+                keys.add(keyPath);
+            }
+        }
+
+        return keys;
+    }
+}
