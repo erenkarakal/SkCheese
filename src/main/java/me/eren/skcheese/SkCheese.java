5d8871f1acb5b239f3425c150c85fb9243331bf5
Eren KARA <karaeren362@gmail.com>
2023-12-28 20:16:14 +0300

    oops

--- src/main/java/me/eren/skcheese/SkCheese.java
+++ src/main/java/me/eren/skcheese/SkCheese.java
@@ -1,6 +1,8 @@
 package me.eren.skcheese;
 
+import ch.njol.skript.Skript;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
 
@@ -11,6 +13,8 @@ public final class SkCheese extends JavaPlugin {
         getLogger().info("Started SkCheese " + getDescription().getVersion());
         new Metrics(this, 19846);
         this.saveDefaultConfig();
+
+        Skript.registerAddon(this);
         for (String key : this.getConfig().getConfigurationSection("syntaxes").getKeys(false)) {
             if (this.getConfig().getBoolean("syntaxes." + key, true)) {
                 registerClass(key);
