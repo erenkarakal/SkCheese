03ab4e12b81756b4d90f98b59ee176cb4fccfb7e
Eren KARA <karaeren362@gmail.com>
2025-05-04 19:25:30 +0300

    remove deprecated listener

--- src/main/java/me/eren/skcheese/elements/labels/ScriptLoadListener.java
+++ src/main/java/me/eren/skcheese/elements/labels/ScriptLoadListener.java
@@ -1,26 +1,15 @@
 package me.eren.skcheese.elements.labels;
 
-import ch.njol.skript.config.Config;
-import ch.njol.skript.events.bukkit.PreScriptLoadEvent;
+import ch.njol.skript.ScriptLoader;
 import me.eren.skcheese.SkCheese;
-import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 
-import java.io.File;
-
-public class ScriptLoadListener implements Listener {
+public class ScriptLoadListener {
 
     static {
-        if (SkCheese.isSyntaxEnabled("labels"))
-            Bukkit.getPluginManager().registerEvents(new ScriptLoadListener(), SkCheese.instance);
-    }
-
-    @EventHandler
-    public void onScriptLoad(PreScriptLoadEvent e) {
-        for (Config config : e.getScripts()) {
-            File scriptFile = config.getFile();
-            LabelStorage.removeScriptLabels(scriptFile);
+        if (SkCheese.isSyntaxEnabled("labels")) {
+            ScriptLoader.eventRegistry().register(ScriptLoader.ScriptPreInitEvent.class, configs ->
+                    configs.forEach(config -> LabelStorage.removeScriptLabels(config.getFile())));
         }
     }
+
 }
