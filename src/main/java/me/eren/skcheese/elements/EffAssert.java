79d047b0e27621feafd1e567d55c4560f63cd1be
Eren Kara <karaeren362@gmail.com>
2026-02-02 22:59:47 +0300

    make EffAssert always stop

--- src/main/java/me/eren/skcheese/elements/EffAssert.java
+++ src/main/java/me/eren/skcheese/elements/EffAssert.java
@@ -20,7 +20,7 @@ import org.skriptlang.skript.registration.SyntaxRegistry;
 import static org.skriptlang.skript.registration.SyntaxInfo.builder;
 
 @Name("Assert")
-@Description("Checks if a condition is true, otherwise throws an error or a warning with a custom message.")
+@Description("Checks if a condition is true, otherwise throws an error or a warning with a custom message and stops the code.")
 @Example("assert {_x} is 1 with error \"{_x} should've been 1\"")
 @Example("assert {_var} > 5000 with warning \"{_var} should've been more than 5000\"")
 @Since("")
@@ -30,7 +30,7 @@ public class EffAssert extends Effect implements RuntimeErrorProducer {
         if (SkCheese.isSyntaxEnabled("assert")) {
             registry.register(SyntaxRegistry.EFFECT,
                     builder(EffAssert.class)
-                            .addPattern("assert <.+> with (:error|warning) [message] %string% [stop:and stop]")
+                            .addPattern("assert <.+> with (:error|warning) [message] %string%")
                             .build()
             );
         }
@@ -38,7 +38,6 @@ public class EffAssert extends Effect implements RuntimeErrorProducer {
 
     private Node node;
     private boolean isError;
-    private boolean shouldStop;
     private Condition condition;
     private Expression<String> message;
 
@@ -46,7 +45,6 @@ public class EffAssert extends Effect implements RuntimeErrorProducer {
     public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
         node = getNode();
         isError = parseResult.hasTag("error");
-        shouldStop = parseResult.hasTag("stop");
 
         String unparsedCondition = parseResult.regexes.getFirst().group();
         condition = Condition.parse(unparsedCondition, null);
@@ -73,10 +71,7 @@ public class EffAssert extends Effect implements RuntimeErrorProducer {
             } else {
                 warning(message);
             }
-
-            if (shouldStop) {
-                return null;
-            }
+            return null;
         }
         return getNext();
     }
